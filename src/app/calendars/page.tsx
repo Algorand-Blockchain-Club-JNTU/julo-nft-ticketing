72c50e5451336fa3e1ccc2a24e9585d66290862a
SamuelRohith <samuelrohith15@gmail.com>
2025-06-18 20:11:10 +0530

    fx : Update page.tsx

--- src/app/calendars/page.tsx
+++ src/app/calendars/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import {  Plus } from "lucide-react"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {  Plus } from "lucide-react";
+import Link from "next/link";
 
 export default function CalendarsPage() {
   return (
