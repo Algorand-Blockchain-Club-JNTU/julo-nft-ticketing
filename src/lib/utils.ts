5816d10f3a085c74c520310861f1094fb54f1d4f
nickthelegend <testingtesla7@gmail.com>
2025-08-21 12:54:00 +0530

    Add logging for inputs length in cn utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,5 +3,6 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
    console.log("cn utility function executed")
+   console.log("Inputs length:", inputs.length)
    return twMerge(clsx(inputs))
 }
